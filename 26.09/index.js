f03e807acb425d316c2bf6dcd7bbae72fa627acb
PiotrMarciniakKcj <90185291+PiotrMarciniakKcj@users.noreply.github.com>
2023-09-26 14:33:53 +0200

    Add files via upload

---
+++ 26.09/index.js
@@ -0,0 +1,16 @@
+const http = require('http')
+
+const port = 3000
+const hostname = '127.0.0.1'
+
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello World!')
+})
+
+server.listen(port, hostname,()=>{
+    console.log(`Server listening. https:${port}:${hostname}`)
+})
+
+// PODJEBANE NA LEKCJI OD PIOTRA ROZCZKI
