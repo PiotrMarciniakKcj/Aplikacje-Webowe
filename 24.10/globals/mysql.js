66fca1259a61e3275bb93a341257c6a079dfe22d
piotrm <piotr.marciniak.kcj@gmail.com>
2023-11-21 15:17:38 +0100

    jest git

--- 24.10/globals/mysql.js
+++
@@ -1,7 +0,0 @@
-export const databaseCredentials = {
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  port: 40000,
-  database: '24_10',
-}
--- 24.10/index.js
+++ 24.10/index.js
@@ -1,10 +1,16 @@
+import http from 'http'
 import express from 'express'
 import bodyParser from 'body-parser'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import fs from 'fs'
+import ejs from 'ejs'
+
 import kontaktRouter from './routes/kontakt.js'
 import apiRouter from './routes/api.js'
 
 const app = express()
-const PORT = 8000
+const port = 3000
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
@@ -17,6 +23,6 @@ app.get('/', (req, res) => {
 app.use('/kontakt', kontaktRouter)
 app.use('/api', apiRouter)
 
-app.listen(PORT, () => {
-  console.log(`server running at port: ${PORT}`)
+app.listen(port, () => {
+  console.log(`server running at port: ${port}`)
 })
